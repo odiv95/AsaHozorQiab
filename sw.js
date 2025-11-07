ae74ccff8a3e9dc3f26d7221d4f9b8cd73cfa504
Omid <72631180+odiv95@users.noreply.github.com>
2025-11-07 21:33:48 +0330

    Update sw.js
    Signed-off-by: Omid <72631180+odiv95@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // نسخه برنامه
-const APP_VERSION = '1.7.12'; // ← هر بار تغییر دادید، فقط این عدد را عوض کنید
+const APP_VERSION = '1.7.13'; // ← هر بار تغییر دادید، فقط این عدد را عوض کنید
 
 // Cache Name بر اساس نسخه برنامه
 const CACHE_NAME = `attendance-app-cache-v${APP_VERSION}`;
@@ -152,9 +152,3 @@ self.addEventListener('message', event => {
         self.skipWaiting();
     }
 });
-
-
-
-
-
-
