2f11d0afabae00488951661a9e3f9cbe10ebd49d
Omid <72631180+odiv95@users.noreply.github.com>
2025-11-07 17:22:19 +0330

    Update vapid-keys.js
    Signed-off-by: Omid <72631180+odiv95@users.noreply.github.com>

--- vapid-keys.js
+++ vapid-keys.js
@@ -1,10 +1,10 @@
 // vapid-keys.js
 const VAPID_KEYS = {
-    publicKey: 'BO3fSiGfLwlDrq97_OEAKwFC1CtabNDP26YkdOEj-oyodVgix7dokcPuX0L3SGk_nPVAq2uRxu5kG7R17jf5S3s',
-    privateKey: '0z2tHbEO7yqQPBtpN-Klxfam8X5ukv-584rZA3q9TMI' // این را محرمانه نگه دارید
+    publicKey: 'BO3fSiGfLwlDrq97_OEAKwFC1CtabNDP26YkdOEj-oyodVgix7dokcPuX0L3SGk_nPVAq2uRxu5kG7R17jf5S3s';
 };
 
 // فقط در محیط سرور از این استفاده کنید
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = VAPID_KEYS;
+
 }
